1248bf41b202948d9015d16b1d51cbcec7e1d533
atarftcode <atarftcode@gmail.com>
2025-12-02 16:05:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-pipeline {
-    agent any
-
-    stages {
-
-        stage('Checkout Source Code') {
-            steps {
-                echo 'Mengambil kode dari GitHub...'
-                git branch: 'main', url: 'https://github.com/atarftcode/KSI2025.git'
-            }
-        }
-
-        stage('Menjalankan PHP') {
-            steps {
-                echo 'Menjalankan file index.php menggunakan PHP...'
-                powershell '''
-                    php index.php
-                '''
-            }
-        }
-
-    }
-}
+<?php
+echo "Jenkins Pipeline berhasil menjalankan index.php!";
+?>
