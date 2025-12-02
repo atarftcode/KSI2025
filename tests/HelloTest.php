0b4c128085bba45ec424757733773d71e3a24e09
atarftcode <atarftcode@gmail.com>
2025-12-02 16:37:51 +0700

    Update HelloTest.php

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,1 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require __DIR__ . '/../hello.php';
 
+class HelloTest extends TestCase {
+
+    public function testSayHello() {
+        $result = sayHello("Atar");
+        $this->assertEquals("Hello, Atar", $result);
+    }
+
+}
+?>
